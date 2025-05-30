68a6d9443376fac89833e3e4a5c2a6a03ca061a9
arlos56 <167236774+arlos56@users.noreply.github.com>
2025-05-30 16:21:36 -0700

    Create HomePage.jsx

---
+++ HomePage.jsx
@@ -0,0 +1,13 @@
+// client/src/pages/HomePage.jsx
+import ProductList from '../components/ProductList';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Product Management</h1>
+      <ProductList />
+    </div>
+  );
+};
+
+export default HomePage;
