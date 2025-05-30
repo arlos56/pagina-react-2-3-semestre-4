b9eb37bf93f4950493f60853753cd2cc4637b125
arlos56 <167236774+arlos56@users.noreply.github.com>
2025-05-30 16:22:51 -0700

    Create EditProductPage.jsx

---
+++ EditProductPage.jsx
@@ -0,0 +1,8 @@
+// client/src/pages/EditProductPage.jsx
+import ProductForm from '../components/ProductForm';
+
+const EditProductPage = () => {
+  return <ProductForm isEdit={true} />;
+};
+
+export default EditProductPage;
