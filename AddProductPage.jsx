d32a605048187b5b324770d3f3ea312d8021c8ac
arlos56 <167236774+arlos56@users.noreply.github.com>
2025-05-30 16:22:03 -0700

    Create AddProductPage.jsx

---
+++ AddProductPage.jsx
@@ -0,0 +1,8 @@
+// client/src/pages/AddProductPage.jsx
+import ProductForm from '../components/ProductForm';
+
+const AddProductPage = () => {
+  return <ProductForm isEdit={false} />;
+};
+
+export default AddProductPage;
